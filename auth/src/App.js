5e11f9a9355005853e1ab4d83633de29085d4588
Angel Cochachez <acochachez@gmail.com>
2021-01-20 00:28:00 -0800

    created js files  for Login and Logout, same as for. env

---
+++ auth/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import LoginButton from './components/LoginButton'
+import LogoutButton from './components/LogoutButton';
+
+function App() {
+  return (
+    <>
+    <LoginButton/>
+    <LogoutButton/>
+      
+    </>
+  );
+}
+
+export default App;
